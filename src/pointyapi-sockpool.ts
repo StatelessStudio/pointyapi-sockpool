e6bd73e2c24726acd2cd9c899630242c66b6dd70
Drew Immerman <drewimmerman@gmail.com>
2019-06-09 21:48:00 -0400

    Created sockpool & tests

--- src/pointyapi-sockpool.ts
+++ src/pointyapi-sockpool.ts
@@ -1,3 +1,135 @@
-export class PointySockpool {}
+import { PointyApi, pointy, jwtBearer } from 'pointyapi';
+import { BaseModel, BaseUser } from 'pointyapi/models';
+
+// Websocket
+import * as socketio from 'socket.io';
+import * as http from 'http';
+
+/**
+ * PointySockpool
+ * 
+ * Create a Websocket Server & User pool in PointyAPI
+ */
+export class PointySockpool {
+	// App
+	public pointyapi: PointyApi;
+	public server: http.Server;
+	public io: socketio.Socket;
+
+	// Websocket pool
+	public sockpool: Object = {};
+	public nSocks: number = 0;
+
+	/**
+	 * Create a sockpool
+	 * @param pointyapi PointyAPI instance to run on
+	 */
+	constructor(pointyapi: PointyApi = pointy) {
+		this.pointyapi = pointyapi;
+	}
+
+	/**
+	 * Initialize sockpool
+	 */
+	public init() {
+		this.server = http.createServer(this.pointyapi.app);
+		this.io = socketio(this.server);
+
+		this.io.on('connection', (client) => {
+			this.registerEvents(client);
+			this.onConnection(client);
+		});
+
+		// Override pointy listen function
+		this.pointyapi.listen = (app, port, log) => {
+			// Startup Websocket Pool
+			this.server.listen(port);
+			console.log(`[SERVER] Listening on port ${port}`);
+		};
+	}
+
+	public registerEvents(client) {}
+
+	/**
+	 * Event - Connection
+	 * @param client Socket client
+	 */
+	public onConnection(client) {
+		client.on('disconnect', (data) => {
+			if (
+				client &&
+				'id' in client &&
+				client.id &&
+				client.id in this.sockpool
+			) {
+				delete this.sockpool[client.id];
+				this.nSocks--;
+			}
+		});
+
+		client.on('auth', async (auth) => {
+			if (client && 'id' in client && client.id) {
+				// Decode token
+				const decoded = jwtBearer.dryVerify(auth.token);
+
+				// Check token
+				if (
+					!('token' in auth) ||
+					!jwtBearer.dryVerify(auth.token) ||
+					!decoded.id ||
+					decoded.id !== auth.userid
+				) {
+					// Token rejected
+					client.emit('auth-fail');
+					return;
+				}
+
+				// Find user
+				const user: void | BaseUser = await this.pointyapi.db.conn
+					.getRepository(this.pointyapi.userType)
+					.findOne({ id: auth.userid })
+					.catch((error) => {
+						this.pointyapi.error(error);
+						client.emit('error', 'Could not authenticate user');
+					});
+
+				// Check user
+				if (!user) {
+					// Couldn't find user
+					client.emit('auth-fail');
+					return;
+				}
+
+				// Add client to sockpool
+				this.sockpool[client.id] = {
+					user: user,
+					socket: client
+				};
+				this.nSocks++;
+
+				// Success
+				client.emit('auth-success', { id: user.id });
+			}
+		});
+	}
+
+	/**
+	 * Push an object to the user
+	 */
+	public push(userId: any, obj: BaseModel) {
+		this.pushRaw(userId, obj.constructor.name, obj);
+	}
+
+	/**
+	 * Push raw data to the user
+	 */
+	public pushRaw(userId: any, label: string, data?: any) {
+		for (const sock in this.sockpool) {
+			if (this.sockpool[sock].user.id === userId) {
+				this.sockpool[sock].socket.emit(label, data);
+			}
+		}
+	}
+}
 
 export const sockpool = new PointySockpool();
---
+++ test/spec/pointyapi-sockpool.spec.ts
@@ -0,0 +1,218 @@
+import 'jasmine';
+import { sockpool, PointySockpool } from '../../src/pointyapi-sockpool';
+import { pointy, jwtBearer } from 'pointyapi';
+import { addResource } from 'pointyapi/utils';
+import { BaseUser, ExampleUser } from 'pointyapi/models';
+import { UserRole } from 'pointyapi/enums';
+
+import * as sockclient from 'socket.io-client';
+const CLIENT_URL = 'http://localhost:8080';
+const sockio = sockclient(CLIENT_URL);
+
+const ROOT_PATH = require('app-root-path').toString();
+
+describe('PointySockpool', () => {
+	beforeAll(async () => {
+		pointy.before = async (app) => {
+			// Database
+			await pointy.db.connect(ROOT_PATH);
+
+			// User
+			await addResource(ExampleUser, {
+				fname: 'Drew',
+				lname: 'Immerman',
+				username: 'drewadmin',
+				password: 'password123',
+				email: 'drewadmin@test.com',
+				role: UserRole.Admin
+			});
+
+			sockpool.registerEvents = (client) => {
+				client.on('testrecv', (data) => client.emit('testsend'));
+				client.on('pushtest', () => {
+					sockpool.pushRaw(1, 'pushtest-complete');
+				});
+				client.on('basemodel', () => {
+					const user = new ExampleUser();
+					user.id = 2;
+					user.username = 'test';
+					sockpool.push(1, user);
+				});
+			};
+
+			// Websocket
+			sockpool.init();
+		};
+
+		// Start server
+		await pointy.start();
+	});
+
+	it('can create a new instance', () => {
+		const sp = new PointySockpool();
+	});
+
+	it('can connect', async () => {
+		await new Promise((a, r) => {
+			sockio.on('connect', () => a());
+		});
+	});
+
+	it('can send/receive messages', async () => {
+		const c = sockclient(CLIENT_URL);
+
+		await new Promise((a, r) => {
+			c.on('connect', () => {
+				c.on('testsend', (data) => {
+					a();
+				});
+				c.emit('testrecv', { hasData: true });
+			});
+
+			c.on('pushtest', () => c.emit('pushtest-complete'));
+		});
+	});
+
+	it('can authenticate', async () => {
+		const c = sockclient(CLIENT_URL);
+		const user = new BaseUser();
+		user.id = 1;
+
+		await new Promise((a, r) => {
+			c.on('connect', () => {
+				c.emit('auth', {
+					userid: 1,
+					token: jwtBearer.sign(user)
+				});
+
+				c.on('auth-success', () => a());
+				c.on('auth-fail', () => r());
+			});
+		});
+	});
+
+	it('checks the token', async () => {
+		const c = sockclient(CLIENT_URL);
+		const user = new BaseUser();
+		user.id = 1;
+
+		await new Promise((a, r) => {
+			c.on('connect', () => {
+				c.emit('auth', {
+					userid: 1,
+					token: 'wrong'
+				});
+
+				c.on('auth-success', () => r());
+				c.on('auth-fail', () => a());
+			});
+		});
+	});
+
+	it('requires token id & user id to match', async () => {
+		const c = sockclient(CLIENT_URL);
+		const user = new BaseUser();
+		user.id = 1;
+
+		await new Promise((a, r) => {
+			c.on('connect', () => {
+				c.emit('auth', {
+					userid: 2,
+					token: jwtBearer.sign(user)
+				});
+
+				c.on('auth-success', () => r());
+				c.on('auth-fail', () => a());
+			});
+		});
+	});
+
+	it('can broadcast to multiple clients', async () => {
+		const c1 = sockclient(CLIENT_URL);
+		const c2 = sockclient(CLIENT_URL);
+		const user = new BaseUser();
+		user.id = 1;
+
+		let isConnected1 = false;
+		let isConnected2 = false;
+
+		let hasResult1 = false;
+		let hasResult2 = false;
+
+		await new Promise((a, r) => {
+			c1.on('connect', () => {
+				c1.emit('auth', {
+					userid: 1,
+					token: jwtBearer.sign(user)
+				});
+
+				c1.on('auth-success', () => {
+					isConnected1 = true;
+
+					if (isConnected2) {
+						// Both connected
+						c1.emit('pushtest');
+					}
+				});
+				c1.on('auth-fail', () => r());
+
+				c1.on('pushtest-complete', () => {
+					hasResult1 = true;
+
+					if (hasResult2) {
+						a();
+					}
+				});
+			});
+
+			c2.on('connect', () => {
+				c2.emit('auth', {
+					userid: 1,
+					token: jwtBearer.sign(user)
+				});
+
+				c2.on('auth-success', () => {
+					isConnected2 = true;
+
+					if (isConnected1) {
+						// Both connected
+						c2.emit('pushtest');
+					}
+				});
+				c2.on('auth-fail', () => r());
+
+				c2.on('pushtest-complete', () => {
+					hasResult2 = true;
+
+					if (hasResult1) {
+						a();
+					}
+				});
+			});
+		});
+	});
+
+	it('can push a BaseModel', async () => {
+		const c = sockclient(CLIENT_URL);
+		const user = new BaseUser();
+		user.id = 1;
+
+		await new Promise((a, r) => {
+			c.on('connect', () => {
+				c.emit('auth', {
+					userid: 1,
+					token: jwtBearer.sign(user)
+				});
+
+				c.on('auth-success', () => {
+					c.on('ExampleUser', (data) => {
+						expect(data.id).toBe(2);
+						a();
+					});
+
+					c.emit('basemodel');
+				});
+			});
+		});
+	});
+});
